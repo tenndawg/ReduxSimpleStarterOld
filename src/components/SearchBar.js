2286fbe591c343224acfcd080aadc826e7c1641f
tenndawg <nickolas.tenney@gmail.com>
2018-04-03 10:12:01 -0600

    section 1 lecture 17

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {term: ''};
+    }
+
+    render() {
+        return(
+            <input onChange={e => console.log(e.target.value)}/>
+        );
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/SearchBar';
+
+const API_KEY = 'AIzaSyA-L0gC31nYSKtJOzkvepBpaZS6HxIlABA';
+
 // Create a new component. This component should produce some HTML.
 const App = () => {
-    return <div>Hi!</div>
+    return ( 
+        <div>
+            <SearchBar/>
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM).
